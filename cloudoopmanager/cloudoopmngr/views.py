05b3fbedd71b6a53d4962f3fc65a3f6351a350cd
jfrodriguez <juanfra.rodriguez.cardoso@gmail.com>
2014-08-11 12:14:09 +0200

    /view-hosts updated

--- cloudoopmanager/cloudoopmngr/views.py
+++ cloudoopmanager/cloudoopmngr/views.py
@@ -73,11 +73,14 @@ def view_hosts(request):
 def infrastructure_overview(request):
     hosts = Host.objects.all()
     datanodes = DataNode.objects.all()
-    hds = HD.objects.all()
-    return render(request, 'infroverview.html', {'datanodes':datanodes,'hosts':hosts, 'hds':hds})
+    return render(request, 'infroverview.html', {'datanodes':datanodes,'hosts':hosts})
 
 def delete_datanode(request, datanode_id):
     datanode = get_object_or_404(DataNode, pk=datanode_id)
+    hds = datanode.hds.all()
+    for hd in hds:
+        hd.status = HD.AVAILABLE
+        hd.save()
     datanode.delete()
     return HttpResponseRedirect('/view-datanodes/')
 
