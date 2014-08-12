6ed441cadf17e234720091347cb7af36770b20fe
danieltardon <danieltardon@gmail.com>
2014-08-12 11:57:25 +0200

    forms

--- cloudoopmanager/cloudoopmngr/forms.py
+++ cloudoopmanager/cloudoopmngr/forms.py
@@ -5,3 +5,13 @@ class NewDataNodeForm(forms.ModelForm):
  
    class Meta:
       model = DataNode
+
+class DataNodeForm(forms.ModelForm):
+    campus = forms.ModelChoiceField(queryset=models.Campus.objects.all())
+    school = forms.ModelChoiceField(queryset=models.School.objects.none()) # Need to populate this using jquery
+    centre = forms.ModelChoiceField(queryset=models.Centre.objects.none()) # Need to populate this using jquery
+
+    class Meta:
+        model = models.Center
+
+        fields = ('campus', 'school', 'centre')
